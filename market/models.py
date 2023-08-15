6c5cac9b8e21b3ed251bfc504d663397f9a5e0e2
arvindh07 <arvindhganesh11@gmail.com>
2023-08-15 14:26:03 +0530

    7 - added user model

--- market/models.py
+++ market/models.py
@@ -1,11 +1,19 @@
 from market import db
 
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(30), nullable=False, unique=True)
+    hash_password = db.Column(db.String(60), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    items = db.relationship('Item', backref='owned_by', lazy=True)
+
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(30), nullable=False, unique=True)
     barcode = db.Column(db.String(12), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False, unique=False)
     description = db.Column(db.String(1024), nullable=False, unique=True)
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
 
     def __repr__(self):
         return f'Item {self.name}'
