5f125a73c6faf17f0abb7ebc1bfc17c47225d6db
arvindh07 <arvindhganesh11@gmail.com>
2023-08-19 11:10:40 +0530

    added basic validations - 9

--- market/forms.py
+++ market/forms.py
@@ -1,9 +1,25 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, DataRequired, Email, EqualTo
 
 class Registration(FlaskForm):
-    username = StringField("Username: ")
-    email = StringField("Email: ")
-    password = PasswordField("Password: ")
-    confirm_password = PasswordField("Confirm password: ")
+    username = StringField("Username: ",
+        validators=[
+            Length(min=3, max=50),
+            DataRequired()
+        ])
+    email = StringField("Email: ",
+        validators=[
+            Email(),
+            DataRequired()
+        ])
+    password = PasswordField("Password: ",
+        validators=[
+            Length(min=6),
+            DataRequired()
+        ])
+    confirm_password = PasswordField("Confirm password: ",
+        validators=[
+            EqualTo('password', message="Password doesn't match")
+        ])
     submit = SubmitField("Create Account")
--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,8 @@
 from market import app
-from flask import render_template
-from market.models import Item
+from flask import render_template, redirect, url_for
+from market.models import Item, User
 from market.forms import Registration
+from market import db
 
 @app.route("/")
 @app.route("/home")
@@ -14,7 +15,18 @@ def market_page():
     items = Item.query.all()
     return render_template("market.html", items=items)
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def registration_page():
     form = Registration()
+    if form.validate_on_submit():
+        # print(f'dta {form.data}')
+        user_to_create = User(username=form.username.data,
+                            hash_password=form.password.data,
+                            email=form.email.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}:
+        for err in form.errors.values():
+            print(f'err {err}')
     return render_template("registration.html", form=form)
