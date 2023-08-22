d942dd93c625c238f4158abeda90cffb38908e9b
arvindh07 <arvindhganesh11@gmail.com>
2023-08-22 07:37:46 +0530

    added logout functionality

--- market/__init__.py
+++ market/__init__.py
@@ -9,5 +9,7 @@ app.config['SECRET_KEY'] = '7c4afa13f72a753f81ee9065'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 
 from market import routes
